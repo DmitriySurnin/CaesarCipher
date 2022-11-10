e91b8327c0161f78f9f698e42f8c78221f8a26a3
DmitriySurnin <117090044+DmitriySurnin@users.noreply.github.com>
2022-11-11 00:26:47 +0300

    Upload

--- src/com/surnin/dmitrii/caesarcodeapp/Encoder.java
+++ src/com/surnin/dmitrii/caesarcodeapp/Encoder.java
@@ -4,38 +4,37 @@ public class Encoder {
 	public static final String ALPHABET_EN = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static final String ALPHABET_RU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
-	// create encryptData() method for encrypting user input string with given shift key
-	public static String encode(String inputStr, int key)
-	{
-		 String encryptStr = "";
-		String ALPHABET;
-		boolean language = Character.UnicodeBlock.of(inputStr.charAt(1)).equals(Character.UnicodeBlock.CYRILLIC)?true:false;
-		if (language)
-		ALPHABET = ALPHABET_EN;
-		else ALPHABET = ALPHABET_RU;
+	public static String encode(String inputStr, int key) {
+		String encryptStr = "";
+		String ALPHABET = "";
+		char c = inputStr.charAt(0);
+		if (Character.UnicodeBlock.of(inputStr.charAt(0)).equals(Character.UnicodeBlock.CYRILLIC)) {
+			ALPHABET = ALPHABET_RU;
+		} else {
+			ALPHABET = ALPHABET_EN;
+		}
 
-		for (int i = 0; i < inputStr.length(); i++)
-		{
-			if (Character.isLetter(inputStr.charAt(i))){
+		for (int i = 0; i < inputStr.length(); i++) {
+			if (Character.isLetter(inputStr.charAt(i))) {
 				int pos = ALPHABET.indexOf(inputStr.charAt(i));
 				int encryptPos = ((key + pos) % ALPHABET.length()); //
 				char encryptChar = ALPHABET.charAt(encryptPos);
 				encryptStr += encryptChar;
-			}
-			else {
-				encryptStr += (char)(inputStr.charAt(i));
+			} else {
+				encryptStr += (char) (inputStr.charAt(i));
 			}
 		}
 		return encryptStr;
 	}
 
-	public static String decode(String encrypted, int key) {
-		boolean language = Character.UnicodeBlock.of(encrypted.charAt(1)).equals(Character.UnicodeBlock.CYRILLIC)?true:false;
-		String ALPHABET;
-		if (language)
+	public static String decode(String encryptStr, int key) {
+		String ALPHABET = "";
+		if (Character.UnicodeBlock.of(encryptStr.charAt(0)).equals(Character.UnicodeBlock.CYRILLIC)) {
+			ALPHABET = ALPHABET_RU;
+		} else {
 			ALPHABET = ALPHABET_EN;
-		else ALPHABET = ALPHABET_RU;
-		int deckey = (ALPHABET.length()-key)%ALPHABET.length();
-		return encode(encrypted, deckey);
+		}
+		int deckey = (ALPHABET.length() - key) % ALPHABET.length();
+		return encode(encryptStr, deckey);
 	}
 }
